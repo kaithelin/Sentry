3a4b5faaadd24d78d21f65d79448654578450316
Vidar Aune Westrum <vidar@dolittle.com>
2018-03-28 15:54:26 +0200

    Started selfregistration styles and restructured

--- Source/Web/Features/app.js
+++ Source/Web/Features/app.js
@@ -10,7 +10,8 @@ export class index {
         config.map([
             { route: ['', 'welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('welcome') },
             { route: 'Accounts/Login', name: 'Login', moduleId: PLATFORM.moduleName('Accounts/Login') },
-            { route: 'Accounts/Consent', name: 'Consent', moduleId: PLATFORM.moduleName('Accounts/Consent') }
+            { route: 'Accounts/Consent', name: 'Consent', moduleId: PLATFORM.moduleName('Accounts/Consent') },
+            { route: 'Registration/SelfRegistration', name: 'SelfRegistration', moduleId: PLATFORM.moduleName('Registration/SelfRegistration') }
         ]);
         this.router = router;
     }
