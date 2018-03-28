c2799dad0793e36ca55a4c282f4bb2b949aa96d8
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-28 17:09:12 +0200

    Enabling push state

--- Source/Web/Features/app.js
+++ Source/Web/Features/app.js
@@ -7,6 +7,7 @@ export class index {
     }
 
     configureRouter(config, router) {
+        config.options.pushState = true;
         config.map([
             { route: ['', 'welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('welcome') },
             { route: 'Accounts/Login', name: 'Login', moduleId: PLATFORM.moduleName('Accounts/Login') }
