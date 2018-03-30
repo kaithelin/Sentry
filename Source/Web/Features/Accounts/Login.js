f81be6302cf1ad0148de224af937ef6937198917
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-30 09:58:11 +0200

    Hooked up getting authorities

--- Source/Web/Features/Accounts/Login.js
+++ Source/Web/Features/Accounts/Login.js
@@ -1,15 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { HttpClient } from 'aurelia-http-client';
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+/**
+ * The view model for the Login view
+ */
+@inject(Router)
 export class Login {
-    authorities = [
-        { id: '0', name: 'Username & Password', url: '', icon: '' },
-        { id: '1', name: 'Google', url: '', icon: '' }
-    ];
+    authorities = []
+    tenant = '';
 
-    id = '';
-
-    constructor() {
+    /**
+     * Initializes a new instance of {Login}
+     * @param {Router} router 
+     */
+    constructor(router) {
+        this.router = router;
     }
 
-    activate(parameters) {
-        this.id = parameters.id;
+    /**
+     * Method that gets called when view and view model is attached.
+     * The reason for using this rather than activate/activated is that this particular feature
+     * is being used as both a stand alone view and can be used as partial to other views
+     */
+    attached() {
+        let self = this;
+        let params = this.router.currentInstruction.params;
+        this.tenant = params.tenant;
+
+        let client = new HttpClient();
+        client.get('/Accounts/Authorities')
+            .then(data => {
+                let authorities = JSON.parse(data.response);
+                authorities.forEach(authority => {
+                    self.authorities.push(authority);
+                });
+            });
     }
 }
