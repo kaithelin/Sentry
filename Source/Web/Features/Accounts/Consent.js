49ba17731f275ef3c29532a52d8817c020068167
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-31 15:47:31 +0200

    Consent up and running

--- Source/Web/Features/Accounts/Consent.js
+++ Source/Web/Features/Accounts/Consent.js
@@ -1,5 +1,63 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { OpenIdConnect } from "aurelia-open-id-connect";
+import { inject } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-http-client';
+import { parseQueryString } from 'aurelia-path';
+
+/**
+ * The view model used for dealing with consent
+ */
+@inject(OpenIdConnect)
 export class Consent {
-    constructor() {
-        
+    information={};
+    rememberConsent=false;
+
+    /**
+     * Initializes a new instance of {Consent}
+     * @param {OpenIdConnect} openIdConnect 
+     */
+    constructor(openIdConnect) {
+        this._openIdConnect = openIdConnect;
+    }
+
+    /**
+     * Method that gets invoked when view and view model is activated
+     */
+    activate() { 
+        let self = this;
+        let client = new HttpClient();
+        let params = parseQueryString(window.location.search.substr(1));
+        this._returnUrl = params.returnUrl;
+        client.createRequest('/Consent')
+            .asGet()
+            .withParams({returnUrl: params.returnUrl})
+            .send()
+            .then(data => {
+                self.information = JSON.parse(data.response);
+                self.information.identityScopes.forEach(scope => scope.checked = true);
+                self.information.resourceScopes.forEach(scope => scope.checked = true);
+            });
+    }
+
+    notAllow() {
+    }
+
+    allow() {
+        let self = this;
+        let scopes = this.information.identityScopes.filter(scope => scope.checked).map(scope => scope.name.value);
+
+        let client = new HttpClient();
+        client.createRequest("/Consent/Grant")
+            .asPost()
+            .withContent(scopes)
+            .withParams(
+                { 
+                    returnUrl: this._returnUrl,
+                    rememberConsent: this.rememberConsent
+                })
+            .send();
     }
 }
