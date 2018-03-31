51e5813fc9cc5f9d7fa9ef35c60defd5bf11278a
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-31 15:42:48 +0200

    Started modelling granting command

---
+++ Source/Domain/Accounts/GrantAccessToApplicationForUser.cs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Domain.Accounts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GrantAccessToApplicationForUser
+    {
+        
+    }
+}
