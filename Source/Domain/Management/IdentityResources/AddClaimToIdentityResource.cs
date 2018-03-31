52596e2a7be8622ca167aefd792ecd8cf6947feb
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-31 15:43:01 +0200

    Fixing namespaces after NS move

--- Source/Domain/Management/IdentityResources/AddClaimToIdentityResource.cs
+++ Source/Domain/Management/IdentityResources/AddClaimToIdentityResource.cs
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using Concepts;
+using Concepts.Claims;
 using Concepts.IdentityResources;
 using Dolittle.Commands;
 
--- Source/Domain/UserManagement/AddClaim.cs
+++ Source/Domain/UserManagement/AddClaim.cs
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using Concepts;
+using Concepts.Claims;
 using Dolittle.Commands;
 
 namespace Domain.UserManagement
