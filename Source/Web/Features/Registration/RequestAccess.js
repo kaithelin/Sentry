8d88e74a29316ea9f039f89d5d8e0dec472d166d
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-31 15:46:38 +0200

    Request access will start login procedure if not logged in

--- Source/Web/Features/Registration/RequestAccess.js
+++ Source/Web/Features/Registration/RequestAccess.js
@@ -1,5 +1,26 @@
+import { OpenIdConnect } from "aurelia-open-id-connect";
+import { inject } from 'aurelia-framework';
+
+@inject(OpenIdConnect)
 export class RequestAccess {
-    constructor() {
+    isLoggedIn=false;
+
+    constructor(openIdConnect) {
+        this._openIdConnect = openIdConnect;
         
+        this._openIdConnect.userManager.getUser()
+            .then((user) => {
+                if (typeof user === "undefined" || user === null) {
+                    this._openIdConnect.login();
+                } else {
+                    if (user.expired) {
+                        this.loginSilent();
+                    } else {
+                        this.inMemoryUser = user;
+                    }
+
+                    this.isLoggedIn = true;
+                }
+            });        
     }
 }
