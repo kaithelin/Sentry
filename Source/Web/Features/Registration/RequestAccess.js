900441677018ed2edc7d4fb8af565ed17be7b447
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-01 11:32:13 +0200

    Cleanup

--- Source/Web/Features/Registration/RequestAccess.js
+++ Source/Web/Features/Registration/RequestAccess.js
@@ -6,57 +6,15 @@ import { Router } from 'aurelia-router';
 const _tenant = new WeakMap();
 const _application = new WeakMap();
 
-
 @inject(Router)
 export class RequestAccess {
     isLoggedIn = false;
 
     constructor(router) {
         this.router = router;
-
-        /*
-        let tenant = '';
-        let application = '';
-        let guidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-        //new Regex("^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$");
-        let segments = window.location.pathname.split('/');
-        if( segments.length > 1 )
-        {
-            if( segments[1].match(guidRegex) ) {
-                tenant = segments[1];
-            }
-        }
-        
-        if( segments.length > 1 && segments[1].match(guidRegex) ) tenant = segments[1];
-        if( segments.length > 2 && segments[2].match(guidRegex) ) application = segments[2];
-        */
-
-
-
-        //this._openIdConnect = openIdConnect;
-
-        /*
-        this._openIdConnect.userManager.getUser()
-            .then((user) => {
-                if (typeof user === "undefined" || user === null) {
-                    this.isLoggedIn = false;
-                } else {
-                    if (user.expired) {
-                        this.loginSilent();
-                    } else {
-                        this.inMemoryUser = user;
-                    }
-
-                    this.isLoggedIn = true;
-                }
-            });        
-        */
     }
-    I
+    
     activate(params, route, navigationInstruction) {
-        
-        
-
         let userStore = new WebStorageStateStore({
             prefix: "requestaccess",
             store: window.localStorage
@@ -73,6 +31,7 @@ export class RequestAccess {
         } else {
 
             // Validate params - guids
+            // let guidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
             _tenant.set(this, params.tenant);
             _application.set(this, params.application);
 
