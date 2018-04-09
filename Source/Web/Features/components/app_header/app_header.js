5892ec1002151b1215b5092d2f7c2faa50c7c3e3
Vidar Aune Westrum <vidar@dolittle.com>
2018-04-09 11:36:37 +0200

    Added an app header with possibility to brand the page.

---
+++ Source/Web/Features/components/app_header/app_header.js
@@ -0,0 +1,4 @@
+export class app_header {
+  logoUrl = 'https://azure.microsoft.com/svghandler/information-protection/?height=70';//70px height, relative width, preferred max 90 px
+  tenantName = "Dolittle Azure";
+}
