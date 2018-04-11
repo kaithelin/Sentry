fc1838750fc5d6e1d92be2aeef7c5c2d5c9be899
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-11 19:19:25 +0200

    Adding DisplayName

--- Source/Read/Management/ProfileClaim.cs
+++ Source/Read/Management/ProfileClaim.cs
@@ -5,18 +5,24 @@
 using System.Collections.Generic;
 using Concepts.Claims;
 using Concepts.Resources;
+using Dolittle.ReadModels;
 
 namespace Read.Management
 {
     /// <summary>
     /// Represents additional claims for a profile - typically collected during registration
     /// </summary>
-    public class ProfileClaim
+    public class ProfileClaim : IReadModel
     {
         /// <summary>
         /// Gets or sets the name of <see cref="Claim"/>
         /// </summary>
         public ClaimName Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the displayed name of <see cref="Claim"/>
+        /// </summary>
+        public string DisplayName { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="ClaimDescription">description</see> of a <see cref="Claim"/>
