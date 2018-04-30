74f12d7a62699c40f3f1f993ae75da307ccf49fb
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-30 15:32:23 +0200

    Cleanup

--- Source/Web/Features/Registration/for_RequestAccess/when_submitting_request.js
+++ Source/Web/Features/Registration/for_RequestAccess/when_submitting_request.js
@@ -1,25 +1,9 @@
 import { RequestAccess } from '../RequestAccess';
-import { test } from '../test.js';
-import { blah } from './given/blah';
-
-//console.log(Command);
 
 describe('when submitting request', () => {
-    let context = new blah();
 
     beforeEach(() => {
         (becauseOf => {
-            //console.log(Reflect.getOwnMetadata);
-            //console.log(context);
-            //console.log(new test().type);
-
-            //var v = new Command();
-            //console.log(Command);
-
-            //let t = new test();
-
-            //let r = new  RequestAccess();
-
         })();
     });
 
