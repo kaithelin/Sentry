230676329d843b06beff992f5f4efde311c9699f
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-30 15:30:37 +0200

    Logo for the Tenant

--- Source/Web/Features/components/app_header/app_header.js
+++ Source/Web/Features/components/app_header/app_header.js
@@ -1,4 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@inject(Router)
 export class app_header {
-  logoUrl = 'https://azure.microsoft.com/svghandler/information-protection/?height=70';//70px height, relative width, preferred max 90 px
-  tenantName = "Dolittle Azure";
+  tenantName = "";
+  tenantId = "";
+
+  /**
+   * Initializes a new instance of {AppHeader}
+   * @param {Router} router 
+   */
+  constructor(router) {
+    this.router = router;
+  }
+
+
+  // https://dolittle.blob.core.windows.net/sentry-tenant-logos/be4c4da6-5ede-405f-a947-8aedad564b7f.png
+  attached() {
+    this.tenantId = this.router.currentInstruction.params.tenant;
+
+    // Todo: We need the tenant name some how!!
+    this.tenantName = this.router.currentInstruction.params.application;
+  }
 }
