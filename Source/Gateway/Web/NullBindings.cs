1ffba5acd22cb1273d1fec3c9b2fdc9aa2cada08
Sindre Wilting <sindrewilting@gmail.com>
2018-08-29 11:06:25 +0200

    Make Gateway compile and run correctly

---
+++ Source/Gateway/Web/NullBindings.cs
@@ -0,0 +1,49 @@
+using System.Globalization;
+using System.Security.Claims;
+using Dolittle.Applications;
+using Dolittle.DependencyInversion;
+using Dolittle.Events.Coordination;
+using Dolittle.Execution;
+using Dolittle.ReadModels;
+using Dolittle.Runtime.Events.Coordination;
+using Dolittle.Runtime.Events.Store;
+using Dolittle.Runtime.Execution;
+using Dolittle.Security;
+using MongoDB.Driver;
+
+namespace Web
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NullBindings : ICanProvideBindings
+    {
+        /// <inheritdoc/>
+        public void Provide(IBindingProviderBuilder builder)
+        {
+            builder.Bind<ICallContext>().To<DefaultCallContext>();
+            builder.Bind<ExecutionContextPopulator>().To((ExecutionContext, details)=> { });
+
+            builder.Bind<ClaimsPrincipal>().To(()=> new ClaimsPrincipal(new ClaimsIdentity()));
+            builder.Bind<CultureInfo>().To(()=> CultureInfo.InvariantCulture);
+
+            builder.Bind<ICanResolvePrincipal>().To(new DefaultPrincipalResolver());
+            
+            var client = new MongoClient("mongodb://localhost:27017"); 
+            var database = client.GetDatabase("Shop_EventStore");
+            builder.Bind<IMongoDatabase>().To(database);
+
+            builder.Bind<IEventStore>().To<Dolittle.Runtime.Events.Store.MongoDB.EventStore>();
+            builder.Bind<IUncommittedEventStreamCoordinator>().To<UncommittedEventStreamCoordinator>();
+
+            builder.Bind<Dolittle.ReadModels.MongoDB.Configuration>().To(new Dolittle.ReadModels.MongoDB.Configuration
+            {
+                Url = "mongodb://localhost:27017",
+                UseSSL = false,
+                DefaultDatabase = "Gateway"
+            });
+
+            builder.Bind(typeof(IReadModelRepositoryFor<>)).To(typeof(Dolittle.ReadModels.MongoDB.ReadModelRepositoryFor<>));
+        }
+    }
+}
