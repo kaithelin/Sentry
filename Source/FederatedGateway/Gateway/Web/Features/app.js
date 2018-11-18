ec4af8ac7cfdf05e4ccb27f3b3afd403d3eef638
Sindre Wilting <sindrewilting@gmail.com>
2018-11-18 15:58:33 +0100

    Fix error in routing

--- Source/FederatedGateway/Gateway/Web/Features/app.js
+++ Source/FederatedGateway/Gateway/Web/Features/app.js
@@ -12,7 +12,7 @@ export class app {
     configureRouter(config, router) {
         config.options.pushState = true;
         config.map([
-            { route: [''][':tenant/:application', ':tenant/:application/welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('welcome'), layoutView: PLATFORM.moduleName('layout_fullscreen.html') },
+            { route: ['', ':tenant/:application', ':tenant/:application/welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('welcome'), layoutView: PLATFORM.moduleName('layout_fullscreen.html') },
             { route: ':tenant/:application/Accounts/Login', name: 'Login', moduleId: PLATFORM.moduleName('Gateway/Accounts/Login') },
             { route: ':tenant/:application/Accounts/Consent', name: 'Consent', moduleId: PLATFORM.moduleName('Gateway/Accounts/Consent') },
             {
