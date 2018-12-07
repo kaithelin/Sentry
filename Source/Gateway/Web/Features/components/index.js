3f64918693905f591a4ea0c966e52f1bb1ed97b1
André Bellefine Lindeløff <andrel@netcompany.com>
2018-12-07 10:15:50 +0100

    Initial config of gateway frontend

---
+++ Source/Gateway/Web/Features/components/index.js
@@ -0,0 +1,6 @@
+import { PLATFORM } from 'aurelia-pal';
+
+export function configure(config) {
+    config.globalResources(PLATFORM.moduleName('./app_footer/app_footer'));
+    config.globalResources(PLATFORM.moduleName('./app_header/app_header'));
+}
