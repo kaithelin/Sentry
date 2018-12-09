c8cf656460595b27a33f4f16fd9e4b5b81197686
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-09 20:37:09 +0100

    Started out tenant association + moving things about

---
+++ Source/Studio/Web/Features/Applications/Tenants/AssociateTenantWithApplication.js
@@ -0,0 +1,16 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class AssociateTenantWithApplication extends Command
+{
+    constructor() {
+        super();
+        this.type = 'ddd115df-b356-4f30-a65a-77e20132b2be';
+
+        this.tenant = '00000000-0000-0000-0000-000000000000';
+        this.application = '00000000-0000-0000-0000-000000000000';
+    }
+}
