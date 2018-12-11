d9a254afa3f933ddcf346b744ddb3a1f733fcbe3
Kai Erland Thelin <kaiert@netcompany.com>
2018-12-11 12:17:11 +0100

    Added event for signup tenant to event horizon

---
+++ Source/Studio/Events.Gateway/SignUps/TenantSignedUp.cs
@@ -0,0 +1,34 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using System;
+using Dolittle.Events;
+
+namespace Events.Gateway.SignUps
+{
+    public class TenantSignedUp : IEvent
+    {
+        public Guid Id { get; }
+        public Guid OwnerId { get; }
+        public string OwnerEmail { get; }
+        public string TenantName { get; }
+        public string HomePage { get; }
+        public Guid CountryId { get; }
+        public string Country { get; }
+        public DateTime SignedUp { get; }
+
+        public TenantSignedUp(Guid id, Guid ownerId, string ownerEmail, string tenantName, string homePage, Guid countryId, string country, DateTime signedUp)
+        {
+            Id = id;
+            OwnerId = ownerId;
+            OwnerEmail = ownerEmail;
+            TenantName = tenantName;
+            HomePage = homePage;
+            CountryId = countryId;
+            Country = country;
+            SignedUp = signedUp;
+        }
+    }
+}
