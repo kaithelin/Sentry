f5207dbf01f656d86ac3a8ffe2a12aa4e611c117
Kai Erland Thelin <kaiert@netcompany.com>
2018-12-13 15:23:44 +0100

    Initial checkin for user invitation

---
+++ Source/Studio/Web/Features/Users/Management/InviteUser.js
@@ -0,0 +1,14 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class InviteUser extends Command
+{
+    constructor() {
+        super();
+        this.type = '27b7cace-1680-4587-9e6f-e4fbd17190dd';
+
+    }
+}
