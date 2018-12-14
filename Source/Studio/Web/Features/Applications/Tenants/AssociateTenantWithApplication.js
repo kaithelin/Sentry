5df588f6e9d8b32c978ce2eb707578c42d2d729d
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-14 11:57:01 +0100

    Correct artifact ID

--- Source/Studio/Web/Features/Applications/Tenants/AssociateTenantWithApplication.js
+++ Source/Studio/Web/Features/Applications/Tenants/AssociateTenantWithApplication.js
@@ -8,7 +8,7 @@ export class AssociateTenantWithApplication extends Command
 {
     constructor() {
         super();
-        this.type = 'ddd115df-b356-4f30-a65a-77e20132b2be';
+        this.type = 'db6077cb-a991-400f-9e8c-3c62f857940d';
 
         this.tenant = '00000000-0000-0000-0000-000000000000';
         this.application = '00000000-0000-0000-0000-000000000000';
--- Source/Studio/Web/Features/Authentication/Configuration/AddOAUTHAuthority.js
+++ Source/Studio/Web/Features/Authentication/Configuration/AddOAUTHAuthority.js
@@ -8,7 +8,7 @@ export class AddOAUTHAuthority extends Command
 {
     constructor() {
         super();
-        this.type = '63db97f0-ed5d-422f-ac24-53c2c24b13d9';
+        this.type = '1311a525-162e-4c00-89c1-800b5e4146ca';
 
         this.application = '00000000-0000-0000-0000-000000000000';
         this.id = '00000000-0000-0000-0000-000000000000';
